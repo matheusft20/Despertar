49060996c20f18c3628daa5371d939d61ef4c37d
Matheus França <75399028+matheusft20@users.noreply.github.com>
2025-06-07 23:56:46 -0300

    Corrigir imagem da Dra. Daniela

--- src/data/psychologists.ts
+++ src/data/psychologists.ts
@@ -26,7 +26,7 @@ export const psychologists: Psychologist[] = [
     specialty: 'Psicodrama',
     description:
       '8 anos de experiência em ansiedade e depressão. Abordagem cognitivo-comportamental.',
-    image: 'dist/assets/images/Dr1.jpeg',
+    image: 'https://images.pexels.com/photos/5327585/pexels-photo-5327585.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1',
     rating: 4.9,
     price: 180,
     badges: ['Online', 'Presencial'],
