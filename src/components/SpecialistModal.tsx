db9fef84cdcbb04c4d3a65978e9372d41a043af5
Matheus França <75399028+matheusft20@users.noreply.github.com>
2025-06-09 00:12:35 -0300

    Updated SpecialistModal.tsx

--- src/components/SpecialistModal.tsx
+++ src/components/SpecialistModal.tsx
@@ -23,7 +23,7 @@ const SpecialistModal: React.FC<SpecialistModalProps> = ({
     <>
       <div className="fixed inset-0 z-50 overflow-y-auto">
         <div className="flex items-center justify-center min-h-screen px-4 pt-4 pb-20 text-center sm:block sm:p-0">
-          <div className="fixed inset-0 transition-opacity bg-gray-500 bg-opacity-75" onClick={onClose}></div>
+          <div className="fixed inset-0 transition-opacity bg-gray-500 bg-opacity-75\" onClick={onClose}></div>
 
           <div className="inline-block w-full max-w-2xl p-6 my-8 overflow-hidden text-left align-middle transition-all transform bg-white shadow-xl rounded-2xl">
             <div className="absolute top-0 right-0 pt-4 pr-4">
